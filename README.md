20725e45251e251f07dabe1a10258e6e25141710
Ashley Yearicks <55463183+ayearicks@users.noreply.github.com>
2020-06-23 15:09:49 -0400

    Initial Upload to Git

---
+++ README.md
@@ -0,0 +1,15 @@
+'Bookmark' Landing Page
+
+This project is a coding challege by Frontend Mentor.
+
+[Frontend Mentor](https://beta.frontendmentor.io) challenges are designed to practice and improve your skills in a real-life workflow.
+
+Technologies used:
+
+HTML
+CSS
+JQUERY
+
+Designed for 375px (mobile portrait) and 1440px (desktops) viewports.
+
+All images provided by https://frontendmentor.io
---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+    let arrowBlue = "assets/img/icon-arrow.svg";
+    let arrowRed = "assets/img/icon-arrowDown.svg";
+    let forms = document.getElementsByClassName('needs-validation');
+
+
+    // Navigation Toggle
+    $(".burger").on("click", function(){
+        $(this).toggleClass("d-none"); 
+        $(".burger-x").toggleClass("d-none");
+        $(".navbar-brand").toggleClass("d-none");
+        $(".navbar-brand2").toggleClass("d-none");
+        $(".navbar").addClass("full-color");
+    });
+
+    $(".burger-x").on("click", function(){
+        $(this).toggleClass("d-none");
+        $(".burger").toggleClass("d-none");
+        $(".navbar-brand").toggleClass("d-none");
+        $(".navbar-brand2").toggleClass("d-none");
+        $(".navbar").removeClass("full-color");
+    });
+
+    // Feature Toggle
+    $(".bookmark").on("click", function(){
+        $(this).toggleClass("current");
+        $(".bookmarkImg").removeClass("d-none");
+        $("#bookmark").removeClass("d-none");
+
+        $(".search").removeClass("current");
+        $(".searchImg").addClass("d-none");
+        $("#search").addClass("d-none");
+
+        $(".share").removeClass("current");
+        $(".shareImg").addClass("d-none");
+        $("#share").addClass("d-none");
+    });
+
+    $(".search").on("click", function(){
+        $(this).toggleClass("current");
+        $(".searchImg").removeClass("d-none");
+        $("#search").removeClass("d-none");
+
+        $(".bookmark").removeClass("current");
+        $(".bookmarkImg").addClass("d-none");
+        $("#bookmark").addClass("d-none");
+
+        $(".share").removeClass("current");
+        $(".shareImg").addClass("d-none");
+        $("#share").addClass("d-none");
+    });
+
+    $(".share").on("click", function(){
+        $(this).toggleClass("current");
+        $(".shareImg").removeClass("d-none");
+        $("#share").removeClass("d-none");
+
+        $(".bookmark").removeClass("current");
+        $(".bookmarkImg").addClass("d-none");
+        $("#bookmark").addClass("d-none");
+
+        $(".search").removeClass("current");
+        $("#search").addClass("d-none");
+        $(".searchImg").addClass("d-none");
+    });
+
+
+
+    // FAQ Toggles
+    $(".btn-faq").on("click", function(){
+        if ($(this).find(".arrowIcon").attr("src") === arrowBlue) {
+            $(this).find(".arrowIcon").attr("src", arrowRed);    
+        } else if ($(this).find(".arrowIcon").attr("src") === arrowRed) {
+            $(this).find(".arrowIcon").attr("src", arrowBlue);
+        }
+    });
+
+    
+    // Invalid Form Submition Toggles
+    let validation = Array.prototype.filter.call(forms, function(form){
+        form.addEventListener('submit', function(e){
+            if (form.checkValidity() === false){
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }, false);
+
+
+});
